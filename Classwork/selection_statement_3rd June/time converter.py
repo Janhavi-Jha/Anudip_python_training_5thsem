454c2aa1eb433f27a98ce5d217e82c00b93e91ff
Janhavi Jha <a2024csd11251@imsec.ac.in>
2026-06-03 14:01:18 +0530

    Python files

---
+++ Classwork/selection_statement_3rd June/time converter.py
@@ -0,0 +1,7 @@
+#program to convert time into corresponding hour,minute and second
+#input time in seconds
+second=int(input("Enter the time in seconds:"))
+#check second is negative or not
+if second<0:
+    exit()
+#-------------------------------------------------------
